f21f56f7569830ba638636a2a54b99d5ea4fdeab
DavidNael <davidnael824@gmail.com>
2022-07-28 16:47:39 +0200

    chapter24

--- lib/Services/auth/auth-service.dart
+++ lib/Services/auth/auth-service.dart
@@ -5,19 +5,22 @@ class AuthService implements AuthProvider {
   final AuthProvider provider;
 
   AuthService(this.provider);
-  
+
   @override
-  Future<AuthUser> createUser({required String email, required String password})=>provider.createUser(email: email, password: password)
-  
+  Future<AuthUser> createUser(
+          {required String email, required String password}) =>
+      provider.createUser(email: email, password: password);
+
   @override
   // TODO: implement currentUser
   AuthUser? get currentUser => provider.currentUser;
-  
+
   @override
-  Future<AuthUser> login({required String email, required String password})=> provider.login(email: email, password: password)
-  
+  Future<AuthUser> login({required String email, required String password}) =>
+      provider.login(email: email, password: password);
+
   @override
-  Future<void> logout() =>provider.logout();
+  Future<void> logout() => provider.logout();
   @override
-  Future<AuthUser> sendEmailVerification() =>provider.sendEmailVerification();
+  Future<AuthUser> sendEmailVerification() => provider.sendEmailVerification();
 }
